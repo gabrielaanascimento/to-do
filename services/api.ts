6b3dfab9bfe28d1f2d6e990ade0a5e61737086cf
Gabriel Augusto <gabrielaugustoassisnascimento@gmail.com>
2026-09-11 08:27:59 -0300

    Update base URL in API configuration
    Added trailing slash to base URL for API.

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || "https://to-do-beta-tan.vercel.app",
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "https://to-do-beta-tan.vercel.app/",
     headers: {
         "Content-Type": "application/json",
     },
